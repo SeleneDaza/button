bb530f4c4ce8ac43a31c0c876f30a6a46de75f3f
SeleneDaza <arian.daza@uptc.edu.co>
2024-08-21 11:10:54 -0500

    finished button

--- src/main/java/co/edu/uptc/presenters/Presenter.java
+++ src/main/java/co/edu/uptc/presenters/Presenter.java
@@ -1,5 +1,16 @@
 package co.edu.uptc.presenters;
 
+import co.edu.uptc.views.Button;
+import co.edu.uptc.views.IoManager;
+
 public class Presenter {
-    
+    public static void main(String[] args) {
+        
+        IoManager ioObject = new IoManager();
+        ioObject.setVisible(true);
+
+        Button button = new Button();
+        button.setVisible(true);
+
+    }
 }
--- src/main/java/co/edu/uptc/views/IoManager.java
+++ src/main/java/co/edu/uptc/views/IoManager.java
@@ -1,5 +1,24 @@
 package co.edu.uptc.views;
 
-public class IoManager {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+// import lombok.AllArgsConstructor;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
+
+// @Setter
+// @Getter
+// @NoArgsConstructor
+// @AllArgsConstructor
+
+public class IoManager extends JFrame{
     
+    public IoManager(){
+        setTitle("ButtonTask");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
 }
